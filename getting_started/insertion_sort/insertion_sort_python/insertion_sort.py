2d6fde308da96f287e819db613c89d0b5198744b
Abdoulaye Diallo <48031880+Vonewman@users.noreply.github.com>
2019-10-11 09:07:11 +0200

    add some changes to insertion_sort.py

--- getting_started/insertion_sort/insertion_sort_python/insertion_sort.py
+++ getting_started/insertion_sort/insertion_sort_python/insertion_sort.py
@@ -1,8 +1,11 @@
-def insertionSort(int a):
+def insertionSort(a):
+    """ Implementation in python of the insertion sort algorithm 
+        - Complexity O(n²)
+    """
     for j in range(1, len(a)):
         key = a[j]
         i = j - 1
-        while (i > 0 and a[i] > key):
+        while (i > -1 and a[i] > key):
             a[i+1] = a[i]
             i = i-1
         a[i+1] = key
