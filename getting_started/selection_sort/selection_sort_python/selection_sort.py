0848cfe27a2ffe9a52f549e33386c3eb4cb8824c
vonewman <vonewman7@gmail.com>
2019-10-28 10:56:25 -0400

    add a other kind of selection sort algorithm in python

--- getting_started/selection_sort/selection_sort_python/selection_sort.py
+++ getting_started/selection_sort/selection_sort_python/selection_sort.py
@@ -4,16 +4,42 @@
 - Complexity O(n²)
 """
 
-def selectionSort(a, n):
-    for i in range(0, n-1):
-        min = i
-        for j in range(i+1, n):
-            if a[j] < a[min]:
-                min = j
-        if min != i:
-            a[i], a[min] = a[min], a[i]        
-    return a
+#def selectionSort(a, n):
+#    for i in range(0, n-1):
+#        min = i
+#        for j in range(i+1, n):
+#            if a[j] < a[min]:
+#                min = j
+#        if min != i:
+#            a[i], a[min] = a[min], a[i]        
+#    return a
 
-arr = [64, 25, 12, 22, 11]
-n = len(arr)
-print(selectionSort(arr, n))
+
+
+def select(T):
+    n = len(T)
+    for i in range(n-1):
+        min_j = i
+        min_x = T[i]
+        for j in range(i+1, n):
+            if T[j] < min_x:
+                min_j = j
+                min_x = T[j]
+        T[min_j] = T[i]
+        T[i] = min_x
+
+def printList(arr):
+    for i in range(len(arr)):
+        print(arr[i], end=" ")
+    print()
+    
+
+if __name__ == "__main__":    
+    arr = [64, 25, 12, 22, 11]
+    print("Given list is ")
+    printList(arr)
+
+    select(arr)
+
+    print("Sorted list is ")
+    printList(arr)
